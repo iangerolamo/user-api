b43f5df1e30393248b2a9762c2d238d32e3cd98c
Ian Gerolamo <iangerolamo@gmail.com>
2021-05-19 09:30:14 -0300

    first commit

---
+++ src/main/java/io/github/iangerolamo/userapi/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package io.github.iangerolamo.userapi.repository;
+
+import io.github.iangerolamo.userapi.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByCpf(String cpf);
+
+    List<User> queryByNomeLike(String nome);
+}
---
+++ src/main/java/io/github/iangerolamo/userapi/service/UserService.java
@@ -0,0 +1,71 @@
+package io.github.iangerolamo.userapi.service;
+
+import io.github.iangerolamo.userapi.dto.UserDTO;
+import io.github.iangerolamo.userapi.model.User;
+import io.github.iangerolamo.userapi.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    // buscar todos os usuários
+
+    public List<UserDTO> getAll() {
+        List<User> usuarios = userRepository.findAll();
+        return usuarios.stream().map(UserDTO::convert).collect(Collectors.toList());
+    }
+
+    // buscar usuário por id
+
+    public UserDTO findById(long userId) {
+        Optional<User> usuario = userRepository.findById(userId);
+        if (usuario.isEmpty()) {
+            return null;
+        }
+        return UserDTO.convert(usuario.get());
+    }
+
+    // salvar um usuário
+
+    public UserDTO save(UserDTO userDTO) {
+        User user = userRepository.save(User.convert(userDTO));
+        return UserDTO.convert(user);
+    }
+
+    // deletar um usuário
+
+    public UserDTO delete(long userId) {
+        Optional<User> user = userRepository.findById(userId);
+        if (user.isPresent()) {
+            userRepository.delete(user.get());
+        }
+        return null;
+    }
+
+    // buscar um usuário por cpf
+
+    public UserDTO findByCpf(String cpf) {
+        User user = userRepository.findByCpf(cpf);
+        if (user != null) {
+            return UserDTO.convert(user);
+        }
+        return null;
+    }
+
+    // buscar usuários por nome
+
+    public List<UserDTO> queryByName(String nome) {
+        List<User> usuarios = userRepository.queryByNomeLike(nome);
+        return usuarios.stream().map(UserDTO::convert).collect(Collectors.toList());
+    }
+}
